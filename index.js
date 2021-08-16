d2e0332a3a5c0590c0be246a5c96bef4f09bb016
roger chat <mattrogerpelosi@gmail.com>
2021-08-16 10:53:36 -0400

    commit tree 09c89e206d7b5045cf1399fa0bcb0ae339c45e85

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const main= document.querySelector('main#main');
+main.remove(); 
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Roger is the champion';
+newHeader.append(); 
